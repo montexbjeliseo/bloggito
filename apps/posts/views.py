f21b63b5f627ee7e4d527d98f55aa68f32be099e
montexbjeliseo <secondmtx@gmail.com>
2023-07-21 13:00:36 -0300

    Delete fakelogin

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,6 +1,4 @@
-from typing import Any, Dict
-from django.http import HttpRequest, HttpResponse
-from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView, FormView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 from .models import *
 from .forms import *
 from django.urls import reverse_lazy
@@ -30,22 +28,3 @@ class PostUpdateView(UpdateView):
     template_name = 'posts/post_update.html'
     success_url = reverse_lazy('posts:index')
     form_class = PostForm
-    
-class FakeLogin(FormView):
-    template_name = 'posts/fake_login.html'
-    
-    def get_success_url(self) -> str:
-        next_url = ''
-        #next_url = self.request.POST["next"]
-        
-        return next_url
-        
-    
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        next_url = ''
-        ctx = super().get_context_data(**kwargs)
-        # if self.request.method == "GET":
-        #     if "next" in self.request.GET:
-        #         next_url = self.request.GET["next"]
-        # ctx["next"] = next_url
-        return ctx
