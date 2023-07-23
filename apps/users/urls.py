f4180e77876039ad52482d5326bc4d2f74a7e860
montexbjeliseo <secondmtx@gmail.com>
2023-07-23 18:09:56 -0300

    Confirm logout when an account sesion is active

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import *
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='auth/login.html'), name = 'login'),    
-    path('logout/', LogoutView.as_view(), name = 'logout'),    
+    path('login/', AuthLoginView.as_view(), name = 'login'),    
+    path('logout/', LogoutView.as_view(), name = 'logout'),  
+    path('confirm_logout', AuthConfirmLogoutView.as_view(), {'next_page': None}, name = 'confirm_logout')  
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
 
-# Create your views here.
+class AuthLoginView(UserPassesTestMixin, LoginView):
+    template_name='auth/login.html'
+    login_url = reverse_lazy('login')
+    
+    def test_func(self):
+        return not self.request.user.is_authenticated
+    
+    def handle_no_permission(self):
+        
+        return redirect(f"{reverse_lazy('confirm_logout')}?next={self.login_url}")
+    
+class AuthConfirmLogoutView(LoginView):
+    template_name = 'auth/confirm_logout.html'
