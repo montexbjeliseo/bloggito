e4c18d04327577a7c5a4b05169eb50f48a9aa9a3
montexbjeliseo <secondmtx@gmail.com>
2023-07-23 23:51:37 -0300

    Post detail

--- blog/settings/settings.py
+++ blog/settings/settings.py
@@ -32,7 +32,13 @@ INSTALLED_APPS = [
     'apps.posts',
     'apps.comments',
     'apps.users',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
