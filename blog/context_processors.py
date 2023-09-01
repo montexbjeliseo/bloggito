ec87a3b5aea81a09f06984b093339d8ba4e062f5
montexbjeliseo <secondmtx@gmail.com>
2023-09-01 16:27:44 -0300

    Site name changed

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -2,7 +2,7 @@ from apps.posts.models import Category
 
 def blog_context(request):
     data = {
-        'app_name': 'Bloggito',
+        'app_name': 'Blogramming',
         'categories': Category.objects.all()
     }
     return data
