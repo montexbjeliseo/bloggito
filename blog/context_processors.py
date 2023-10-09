a057d5b9d63da72fff43d4b1ee87c905e454bc90
montexbjeliseo <secondmtx@gmail.com>
2023-10-08 22:59:29 -0300

    back to home in confirm logout

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,8 +1,11 @@
 from apps.posts.models import Category
+from django.urls import reverse
+
 
 def blog_context(request):
     data = {
         'app_name': 'Blogramming',
-        'categories': Category.objects.all()
+        'categories': Category.objects.all(),
+        'go_home': reverse('index'),
     }
     return data
